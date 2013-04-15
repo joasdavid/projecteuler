da96304f2f3c07f43760bb80a708fb13c7a39b21
Joás V. Pereira <joasdavid@gmail.com>
2013-04-16 00:43:06 +0100

    p1 final

--- C solution/p1/file.c
+++ C solution/p1/file.c
@@ -16,12 +16,16 @@
  	pid_t pid;
  	int max = 1000;
  	int soma = 0;
+ 	int dados[2];
+ 	int buf;
  	int i, estado;
 
+ 	pipe(dados);
  	pid = fork();
  	if(pid>0){
+ 		close(dados[1]);
  		/*processo pai*/
- 		for (i = 1; i < (max/2); i++)
+ 		for (i = 1; i < (max/2)-1; i++)
  		{
  			if ((i % 3 == 0) || (i % 5 == 0))
  			{
@@ -29,16 +33,22 @@
  			}
  		}
  		wait(&estado);
- 		soma+=WEXITSTATUS(estado);
+ 		read(dados[0],&buf,sizeof(int));
+ 		close(dados[0]);
+ 		soma+=buf;
  	}else{
  		/*processo filho*/
+ 		close(dados[0]);
  		for (i = (max/2); i < max; i++)
  		{
  			if ((i % 3 == 0) || (i % 5 == 0))
  			{ 				soma+=i;
  			}
  		}
- 		exit(soma);
+ 		buf = soma;
+ 		write(dados[1],&buf,sizeof(int));
+ 		close(dados[1]);
+ 		exit(pid);
  	}
 
  	printf("%d\n", soma);
